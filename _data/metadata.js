e94dda6cec92006742d596195058304da87a5391
Helen Burgess <me@helenburgess.id.au>
2024-02-28 16:42:32 +0800

    first commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Helen Burgess",
+	url: "https://helenburgess.id.au",
+	language: "en",
+	description: "I am writing about my experiences as a naval navel-gazer.",
+	author: {
+		name: "Helen Burgess",
+		email: "me@helenburgess.id.au",
+		url: "https://helenburgess.id.au/my-story/",
+	},
+};
