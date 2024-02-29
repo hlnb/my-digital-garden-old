99f7d14aa4ac95ffe7e8c2da303350740e5d40fc
Helen Burgess <me@helenburgess.id.au>
2024-02-29 14:47:25 +0800

    changed themes yet again. Like this one.

--- .eleventy.js
+++ .eleventy.js
@@ -1,33 +1,70 @@
-const Image = require('@11ty/eleventy-img');
-
-async function imageShortcode(src, alt, sizes) {
-	let metadata = await Image(`./src${src}`, {
-		widths: [300, 800, null],
-		formats: ['avif', 'jpeg'],
-		urlPath: '/images/',
-		outputDir: './public/images/',
-	});
-
-	let imageAttributes = {
-		alt,
-		sizes,
-		loading: 'lazy',
-		decoding: 'async',
-	};
-
-	return Image.generateHTML(metadata, imageAttributes);
-}
+const { DateTime } = require("luxon");
+// Import Nunjucks
+//const Nunjucks = require("nunjucks");
+// excerpt of my .eleventy.js config file
+const CleanCSS = require("clean-css");
+const { minify } = require("terser");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy('./src/css/');
-	eleventyConfig.addWatchTarget('./src/css/');
-	eleventyConfig.addPassthroughCopy('./src/images/');
-	eleventyConfig.addPassthroughCopy({ './src/favicons': '/' });
-	eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
-	eleventyConfig.addNunjucksAsyncShortcode('EleventyImage', imageShortcode);
-	return {
-		dir: {
-			input: 'src',
-			output: 'public',
-		},
-	};
+  // Add plugins
+  eleventyConfig.addPlugin(syntaxHighlight);
+  // e.g. Use syntax highlighters in njk and md Eleventy templates (not liquid)
+  templateFormats: ["html", "md"],
+    //Pass through files
+    eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/.well-known");
+  eleventyConfig.addPassthroughCopy("./robots.txt");
+
+  //Date Clean up
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+  //Latests Summary Limit
+  eleventyConfig.addFilter("limit_latest", function (arr, limit) {
+    return arr.slice(0, limit);
+  });
+  //Post Summary Limit
+  eleventyConfig.addFilter("limit", function (arr, limit) {
+    return arr.slice(1, limit);
+  });
+  // remove snippet from build
+  eleventyConfig.setBrowserSyncConfig({
+    snippet: false,
+  });
+  // minify css in build
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+  // minify js on build
+  eleventyConfig.addNunjucksAsyncFilter(
+    "jsmin",
+    async function (code, callback) {
+      try {
+        const minified = await minify(code);
+        callback(null, minified.code);
+      } catch (err) {
+        console.error("Terser error: ", err);
+        // Fail gracefully.
+        callback(null, code);
+      }
+    }
+  );
+
+  //https://stackoverflow.com/questions/32138820/remove-div-tags-and-replace-some-of-them-by-br-tag
+  // excerpt filter
+  eleventyConfig.addFilter("excerpt", (post) => {
+    let text = post;
+    let result = text.replace(/<figure>.*<\/figure>/s, "");
+    //return result;
+    let content = result.replace(/(<([^>]+)>)/gi, "");
+    return content.substr(0, content.lastIndexOf(" ", 184));
+  });
+
+  // Return your Object options:
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
 };
